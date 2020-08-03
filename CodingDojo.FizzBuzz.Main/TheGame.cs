2a2f3c00b5929d41810d5226a67c26971fe67205
suligapawel <psul00@me.pl>
2020-08-03 21:06:43 +0200

    First test

--- CodingDojo.FizzBuzz.Main/TheGame.cs
+++ CodingDojo.FizzBuzz.Main/TheGame.cs
@@ -4,5 +4,12 @@ namespace CodingDojo.FizzBuzz.Main
 {
     public class TheGame
     {
+        private int _counter;
+
+        public string Count()
+        {
+            _counter++;
+            return _counter.ToString();
+        }
     }
 }
--- CodingDojo.FizzBuzz.Test/TheGameTest.cs
+++ CodingDojo.FizzBuzz.Test/TheGameTest.cs
@@ -1,18 +1,26 @@
+using CodingDojo.FizzBuzz.Main;
 using NUnit.Framework;
 
 namespace CodingDojo.FizzBuzz.Test
 {
     public class Tests
     {
+        private TheGame _theGame;
+
         [SetUp]
         public void Setup()
         {
+            _theGame = new TheGame();
         }
 
         [Test]
-        public void Test1()
+        public void When_NextNumberIsNotDivisibleBy3And5_return_thisNumber()
         {
-            Assert.Pass();
+            string result = _theGame.Count();
+            string nextResult = _theGame.Count();
+
+            Assert.That(result, Is.EqualTo("1"));
+            Assert.That(nextResult, Is.EqualTo("2"));
         }
     }
 }
