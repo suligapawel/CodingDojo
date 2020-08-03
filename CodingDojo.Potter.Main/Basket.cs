b6ac8b48639eb036e28bc0dd1e5cf829a5786f0f
suligapawel <psul00@me.pl>
2020-08-03 22:36:04 +0200

    Buy one copy of book

--- CodingDojo.Potter.Main/Basket.cs
+++ CodingDojo.Potter.Main/Basket.cs
@@ -1,8 +1,14 @@
-﻿using System;
-
-namespace CodingDojo.Potter.Main
+﻿namespace CodingDojo.Potter.Main
 {
     public class Basket
     {
+        public void InsertBook()
+        {
+        }
+
+        public int Buy()
+        {
+            return 8;
+        }
     }
 }
--- CodingDojo.Potter.Test/BasketTest.cs
+++ CodingDojo.Potter.Test/BasketTest.cs
@@ -1,18 +1,26 @@
+using CodingDojo.Potter.Main;
 using NUnit.Framework;
 
 namespace CodingDojo.Potter.Test
 {
     public class Tests
     {
+        private Basket _basket;
+
         [SetUp]
         public void Setup()
         {
+            _basket = new Basket();
         }
 
         [Test]
-        public void Test1()
+        public void When_BuyOneCopyOfBook_return_8euro()
         {
-            Assert.Pass();
+            _basket.InsertBook();
+
+            int cost = _basket.Buy();
+
+            Assert.That(cost, Is.EqualTo(8));
         }
     }
 }
